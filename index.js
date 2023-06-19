379923acb1ca1ee21fc874bab79015639d5c1b74
rywilldev <rywilldav@gmail.com>
2023-06-19 15:12:15 -0400

    update index.js
    add func getFeedHtml

--- index.js
+++ index.js
@@ -2,8 +2,40 @@ import { tweetsData } from "./data.js";
 
 const tweetInput = document.getElementById("tweet-input");
 const tweetBtn = document.getElementById("tweet-btn");
+const tweetFeed = document.getElementById("feed");
 
-tweetBtn.addEventListener("click", function(){
-    console.log(tweetInput.value);
-    tweetInput.value = "";
-})
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value);
+  tweetInput.value = "";
+});
+
+function getFeedHtml() {
+  let feedHtml = "";
+  for (let tweet of tweetsData) {
+    feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="./images/${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>
+        `;
+  }
+  return feedHtml;
+}
+
+getFeedHtml();
